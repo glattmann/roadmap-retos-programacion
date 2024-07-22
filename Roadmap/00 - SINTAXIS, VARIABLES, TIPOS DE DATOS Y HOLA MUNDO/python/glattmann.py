3933b1e0af831d02e1eba8112c70312b18b76eec
glattmann <79729823+glattmann@users.noreply.github.com>
2024-07-22 19:52:31 -0300

    Create glattmann.py
    Reto-#00

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/glattmann.py
@@ -0,0 +1,23 @@
+# URL oficial del lenguaje seleccionado: https://www.python.org/
+
+#Comentario de una línea
+
+"""Comentario de 
+múltiples líneas"""
+
+#Creando una variable
+variable1 = "" #Creamos una variable con una cadena vacía.
+
+#Python no puede crear constantes, pero por convención se utiliza el nombre de las variables en MAYÚSCULAS para representarlas.
+
+PI = 3.14159
+
+#Creando variables con datos primitivos
+cadena = "Soy una cadena"
+entero = 10
+flotante = 10.0
+booleano = True
+nulo = None
+
+#Imprimiendo en pantalla ¡Hola, Python!
+print("¡Hola, Python!")
